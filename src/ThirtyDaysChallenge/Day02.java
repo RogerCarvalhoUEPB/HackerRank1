05605557ba301d599382bac70936fa2691d98078
Roger.Carvalho <crogernatan@gmail.com>
2024-05-16 16:51:30 -0300

    Adicionando dia 02

--- src/ThirtyDaysChallenge/Day02.java
+++ src/ThirtyDaysChallenge/Day02.java
@@ -1,10 +1,21 @@
 package ThirtyDaysChallenge;
 
+import java.util.Scanner;
 public class Day02 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Testando... Testandooo");
-	}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Scanner scan = new Scanner(System.in);
+	int a, i=4;
+	double b, d=4.0;
+	String c, s = "HackerRank";
+	a = scan.nextInt();
+	b = scan.nextDouble();
+	c = scan.nextLine();
+	System.out.println(i+a);
+	System.out.printf("\n %f", d+b);
+	System.out.println(s+c);
+	scan.close();
+}
 
 }
