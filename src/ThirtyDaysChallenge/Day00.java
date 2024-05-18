1ccf547b4196162fe863f15184041701f69abefc
Roger.Carvalho <crogernatan@gmail.com>
2024-05-18 00:08:18 -0300

    Corrigindo nomes

---
+++ src/ThirtyDaysChallenge/Day00.java
@@ -0,0 +1,14 @@
+package ThirtyDaysChallenge;
+import java.util.*;
+
+public class Day01 {
+
+    public static void main(String[] args) {
+    	 /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String inputString;
+        inputString = scan.nextLine();
+        scan.close();
+        System.out.print("Hello, World.\n"+inputString);
+           }
+}
--- src/ThirtyDaysChallenge/Day01.java
+++ src/ThirtyDaysChallenge/Day01.java
@@ -1,14 +1,21 @@
 package ThirtyDaysChallenge;
-import java.util.*;
 
+import java.util.Scanner;
 public class Day01 {
 
-    public static void main(String[] args) {
-    	 /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scan = new Scanner(System.in);
-        String inputString;
-        inputString = scan.nextLine();
-        scan.close();
-        System.out.print("Hello, World.\n"+inputString);
-           }
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Scanner scan = new Scanner(System.in);
+	int a, i=4;
+	double b, d=4.0;
+	String c, s = "HackerRank";
+	a = scan.nextInt();
+	b = scan.nextDouble();
+	c = scan.nextLine();
+	System.out.println(i+a);
+	System.out.printf("\n.1%f", d+b);
+	System.out.println(s+c);
+	scan.close();
+}
+
 }
